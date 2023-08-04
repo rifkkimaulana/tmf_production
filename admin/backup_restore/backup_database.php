9bc0c0644d7149870257d0e8439a4a47ba976f5f
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-05 03:14:49 +0700

    update artikel back

--- admin/backup_restore/backup_database.php
+++ admin/backup_restore/backup_database.php
@@ -30,7 +30,6 @@ foreach ($tables as $table) {
     $backupContent .= "\n";
 }
 
-// Set header untuk mengunduh file tanpa menyimpannya di direktori
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename=' . basename($backupFileName));
