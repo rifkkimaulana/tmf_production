3fb7814376bcc5cd460d69c3f75edb65f67f1010
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-05 22:03:25 +0700

    add modal notifikasi

--- login/modal.php
+++ login/modal.php
@@ -1,15 +1,16 @@
-<div class="modal fade" id="tokenExpired" tabindex="-1" role="dialog" aria-labelledby="tokenExpiredModalLabel"
+<!-- Modal Page Google & Login -->
+<div class="modal fade" id="userNot_found" tabindex="-1" role="dialog" aria-labelledby="userNot_foundModalLabel"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="tokenExpiredModalLabel">Token Expired</h5>
+                <h5 class="modal-title" id="userNot_foundModalLabel">User Tidak Ditemukan!</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                Token Anda telah kedaluwarsa. Silakan coba kembali untuk mereset password.
+                Akun pengguna tidak tersedia, silahkan login dengan pengguna lain.
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
@@ -18,59 +19,38 @@
     </div>
 </div>
 
-<div class="modal fade" id="gagalLoginModal" tabindex="-1" role="dialog" aria-labelledby="gagalLoginModalLabel"
+<div class="modal fade" id="userLevel_notFound" tabindex="-1" role="dialog"
+    aria-labelledby="userLevel_notFoundModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="userLevel_notFoundModalLabel">Gagal Verifikasi Pengguna</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Autentikasi ke data pengguna tidak dapat diproses.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="userNotFound" tabindex="-1" role="dialog" aria-labelledby="userNotFoundModalLabel"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="gagalLoginModalLabel">Gagal Login</h5>
+                <h5 class="modal-title" id="userNotFoundModalLabel">Gagal Verifikasi Pengguna</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                Username atau password yang Anda masukkan salah.
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="passwordMismatchModal" tabindex="-1" role="dialog"
-    aria-labelledby="passwordMismatchModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="passwordMismatchModalLabel">Peringatan!</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Password tidak cocok. Harap pastikan password dan konfirmasi password sesuai. Silahkan buka kembali link
-                yang dikirimkan melalui whatsapp!.
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="passwordSuccessModal" tabindex="-1" role="dialog"
-    aria-labelledby="passwordSuccessModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="passwordSuccessModalLabel">Password Berhasil Diupdate</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Password Anda berhasil diupdate.
+                Pengguna yang anda pilih tidak tersedia.
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
@@ -84,13 +64,13 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="emailNotFoundModalLabel">Email Tidak Terdaftar</h5>
+                <h5 class="modal-title" id="emailNotFoundModalLabel">Email Google Tidak Tersedia.</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                Email yang Anda masukkan tidak terdaftar.
+                Email tidak tersedia.
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
@@ -99,18 +79,38 @@
     </div>
 </div>
 
-<div class="modal fade" id="failedToken" tabindex="-1" role="dialog" aria-labelledby="failedTokenModalLabel"
+<div class="modal fade" id="accesTokenNotFound" tabindex="-1" role="dialog"
+    aria-labelledby="accesTokenNotFoundModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="accesTokenNotFoundModalLabel">Akses Token Error.</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Tidak dapat login karena kesalahan akses token google.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="codeNotFound" tabindex="-1" role="dialog" aria-labelledby="codeNotFoundModalLabel"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="failedTokenModalLabel">Failed Token</h5>
+                <h5 class="modal-title" id="codeNotFoundModalLabel">Akses Kode Error.</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                Token gagal diverifikasi.
+                Kesalahan akses kode silahkan hubungi developer aplikasi.
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
@@ -119,38 +119,38 @@
     </div>
 </div>
 
-<div class="modal fade" id="invalidResponseModal" tabindex="-1" role="dialog"
-    aria-labelledby="invalidResponseModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="invalidResponseModalLabel">Invalid Response</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Response tidak valid.
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="emailNotFound" tabindex="-1" role="dialog" aria-labelledby="emailNotFoundModalLabel"
+<div class="modal fade" id="passwordSalah" tabindex="-1" role="dialog" aria-labelledby="passwordSalahModalLabel"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="emailNotFoundModalLabel">Error</h5>
+                <h5 class="modal-title" id="passwordSalahModalLabel">Password Salah!</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p>Email tidak ditemukan. Silakan coba lagi.</p>
+                Password atau username yang anda masukan tidak tersedia.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="sendSukses" tabindex="-1" role="dialog" aria-labelledby="sendSuksesModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="sendSuksesModalLabel">Berhasil Terkirim</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Permintaan lupa password anda berhasil kami kirim. silahkan cek pesan baru di whatsapp!.</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
@@ -160,23 +160,62 @@
 </div>
 
 
-<div class="modal fade" id="emailSuccessModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="suksesUpdatePassword" tabindex="-1" aria-labelledby="suksesUpdatePasswordModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Success</h5>
+                <h5 class="modal-title" id="suksesUpdatePasswordModalLabel">Success</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                Email berhasil diverifikasi! Silahkan cek whatsapp anda kami telah mengirimkan link reset password!
+                Password Berhasil diubah silahkan login dengan password baru anda.
             </div>
             <div class="modal-footer">
                 <a href="<?php echo $base_url; ?>" class="btn btn-primary">Masuk</a>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
             </div>
-
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="passwordMismatch" tabindex="-1" aria-labelledby="passwordMismatchModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="passwordMismatchModalLabel">Password Tidak Sesuai!</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Password yang anda masukan tidak sesuai.
+            </div>
+            <div class="modal-footer">
+                <a href="<?php echo $base_url; ?>" class="btn btn-primary">Masuk</a>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="tokenExpired" tabindex="-1" aria-labelledby="tokenExpiredModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tokenExpiredModalLabel">Token Kadaluarsa!</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Link whatsapp sudah tidak dapat digunakan!. silahkan kirim ulang permintaan lupa password.
+            </div>
+            <div class="modal-footer">
+                <a href="<?php echo $base_url; ?>" class="btn btn-primary">Masuk</a>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
         </div>
     </div>
 </div>
@@ -184,30 +223,30 @@
 <script>
     $(document).ready(function () {
         const urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.get('success') === 'email_berhasil_diverifikasi') {
-            $('#emailSuccessModal').modal('show');
-        }
-    });
-</script>
-<script>
-    $(document).ready(function () {
-        const urlParams = new URLSearchParams(window.location.search);
         const alertParam = urlParams.get('alert');
 
-        if (alertParam === 'gagal') {
-            $('#gagalLoginModal').modal('show');
-        } else if (alertParam === 'password_tidak_sesuai') {
-            $('#passwordMismatchModal').modal('show');
-        } else if (alertParam === 'berhasil_update_password') {
-            $('#passwordSuccessModal').modal('show');
-        } else if (alertParam === 'email_tidak_terdaftar') {
+        if (alertParam === 'userNot_found') {
+            $('#userNot_found').modal('show');
+        } else if (alertParam === 'userLevel_notFound') {
+            $('#userLevel_notFound').modal('show');
+        } else if (alertParam === 'userNotFound') {
+            $('#userNotFound').modal('show');
+        } else if (alertParam === 'emailNotFound') {
             $('#emailNotFound').modal('show');
-        } else if (alertParam === 'invalid_response') {
-            $('#invalidResponseModal').modal('show');
-        } else if (alertParam === 'token_expired') {
+        } else if (alertParam === 'accesTokenNotFound') {
+            $('#accesTokenNotFound').modal('show');
+        } else if (alertParam === 'codeNotFound') {
+            $('#codeNotFound').modal('show');
+        } else if (alertParam === 'passwordSalah') {
+            $('#passwordSalah').modal('show');
+        } else if (alertParam === 'sendSukses') {
+            $('#sendSukses').modal('show');
+        } else if (alertParam === 'suksesUpdatePassword') {
+            $('#suksesUpdatePassword').modal('show');
+        } else if (alertParam === 'passwordMismatch') {
+            $('#passwordMismatch').modal('show');
+        } else if (alertParam === 'tokenExpired') {
             $('#tokenExpired').modal('show');
-        } else if (alertParam === 'failed_token') {
-            $('#failedToken').modal('show');
         }
     });
 </script>
