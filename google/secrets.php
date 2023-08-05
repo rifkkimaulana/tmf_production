4ea05c33a6710ce7c7c6707e7b28132e0bce3949
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-05 20:08:18 +0700

    update uri

--- google/secrets.php
+++ google/secrets.php
@@ -1,6 +1,6 @@
 <?php
 include '../config/base_url.php';
-$tujuan_uri = '/google/callback.php';
+$tujuan_uri = '/callback.php';
 
 $client_id = '228391422070-4rb2h00mobtj029r29lipij8rqtg982r.apps.googleusercontent.com';
 $client_secret = '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';
