4cf98880221828beaeaeba233ee0ba34e7fc2109
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-05 14:03:35 +0700

    update koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -10,5 +10,5 @@ $koneksi = mysqli_connect($host, $username, $password, $database);
 if (!$koneksi) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 } else {
-    //echo 'Koneksi Berhasil';
+    echo 'Koneksi Berhasil';
 }
