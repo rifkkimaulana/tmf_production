d40d10fe316cad6ce24fb1c6535c2c7db4da2cc1
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-05 19:55:53 +0700

    update secrets

--- google/secrets.php
+++ google/secrets.php
@@ -2,7 +2,7 @@
 include '../config/base_url.php';
 $tujuan_uri = $base_url . '/google/callback.php';
 
-$client_id = '864273206547-25t6ouvbg3lmp0fl1igi1p4i8chudbam.apps.googleusercontent.com';
-$client_secret = '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';
+$client_id = '228391422070-4rb2h00mobtj029r29lipij8rqtg982r.apps.googleusercontent.com';
+$client_secret = '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';
 $redirect_uri = $tujuan_uri;
 ?>
