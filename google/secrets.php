b05239f513f77ac77b4acc766e4df6427acb5a88
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-05 19:33:47 +0700

    updadte redirect uri

--- google/secrets.php
+++ google/secrets.php
@@ -4,5 +4,5 @@ $tujuan_uri = $base_url . '/google/callback.php';
 
 $client_id = '864273206547-25t6ouvbg3lmp0fl1igi1p4i8chudbam.apps.googleusercontent.com';
 $client_secret = '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';
-$redirect_uri = $base_url . $tujuan_uri;
+$redirect_uri = $tujuan_uri;
 ?>
