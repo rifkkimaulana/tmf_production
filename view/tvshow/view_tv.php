0bec74a6fdc6fe5f9078830b98ff111c1d7b0af8
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-06 01:26:24 +0700

    update

--- view/tvshow/view_tv.php
+++ view/tvshow/view_tv.php
@@ -11,10 +11,8 @@ $query_tmdb = "SELECT link_trailer FROM tb_tmdb WHERE id = '$tv_tmdb_id'";
 $result_tmdb = mysqli_query($koneksi, $query_tmdb);
 if (mysqli_num_rows($result_tmdb) > 0) {
     $row_tmdb = mysqli_fetch_assoc($result_tmdb);
-    $link = !empty($row_tmdb['link_trailer']) ? $row_tmdb['link_trailer'] : $row_film['link_trailer'];
+    $link = $row_tmdb['link_trailer'];
 }
-
-
 
 $query_episode = "SELECT * FROM tb_episode_tv_show WHERE tv_show_id = '$id_tv'";
 $result_episode = mysqli_query($koneksi, $query_episode);
