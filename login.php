840e39c90064f08afd3da308c4b42cea42b62bbf
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-05 19:05:54 +0700

    update class login page

--- login.php
+++ login.php
@@ -66,56 +66,58 @@ if (isset($_COOKIE['remember_me'])) {
 <?php include 'halaman/header.php'; ?>
 <div class="login-page">
     <div class="login-box">
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="<?php echo $base_url; ?>" class="h1"><b>TMF</b>PRODUCTION</a>
-            </div>
-            <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-                <div class="social-auth-links text-center mt-2 mb-3">
-                    <a href="<?php echo $base_url; ?>/google/login.php" class="btn btn-block btn-danger">
-                        <i class="fab fa-google mr-2"></i> Sign in using Google
-                    </a>
+        <div class="card-flat">
+            <div class="card card-outline card-primary">
+                <div class="card-header text-center">
+                    <a href="<?php echo $base_url; ?>" class="h1"><b>TMF</b>PRODUCTION</a>
                 </div>
-                <h5 class="text-center">
-                    ATAU
-                </h5>
-                <form action="" method="post">
-                    <div class="input-group mb-3">
-                        <input type="username" class="form-control" placeholder="Username" name="username" required
-                            autofocus>
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
+                <div class="card-body">
+                    <p class="login-box-msg">Sign in to start your session</p>
+                    <div class="social-auth-links text-center mt-2 mb-3">
+                        <a href="<?php echo $base_url; ?>/google/login.php" class="btn btn-block btn-danger">
+                            <i class="fab fa-google mr-2"></i> Sign in using Google
+                        </a>
+                    </div>
+                    <h5 class="text-center">
+                        ATAU
+                    </h5>
+                    <form action="" method="post">
+                        <div class="input-group mb-3">
+                            <input type="username" class="form-control" placeholder="Username" name="username" required
+                                autofocus>
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" name="password" required>
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password" name="password" required>
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember" name="remember" <?php if ($remember)
-                                    echo "checked"; ?>>
-                                <label for="remember">
-                                    Remember Me
-                                </label>
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="icheck-primary">
+                                    <input type="checkbox" id="remember" name="remember" <?php if ($remember)
+                                        echo "checked"; ?>>
+                                    <label for="remember">
+                                        Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary btn-block" name="submit">Sign In</button>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block" name="submit">Sign In</button>
-                        </div>
-                    </div>
-                </form>
-                <p class="mt-1">
-                    <a href="<?php echo $base_url; ?>/insert_email.php">I forgot my password</a>
-                </p>
+                    </form>
+                    <p class="mt-1">
+                        <a href="<?php echo $base_url; ?>/insert_email.php">I forgot my password</a>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
