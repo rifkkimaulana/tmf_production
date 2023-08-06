c538b6ee6b9a526f04719525a51daaa0c4941d6d
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-07 01:24:32 +0700

    u

--- halaman/main-content.php
+++ halaman/main-content.php
@@ -1,3 +1,40 @@
+<!-- Tombol untuk memicu modal -->
+<button type="button" class="btn btn-primary" onclick="showLoadingModal()">Tampilkan Loading</button>
+
+<!-- Modal -->
+<div id="loadingModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <!-- Konten modal -->
+        <div class="modal-content">
+            <!-- Animasi loading -->
+            <div class="overlay">
+                <i class="fa fa-refresh fa-spin"></i>
+            </div>
+            <!-- Header modal -->
+            <div class="modal-header">
+                <h4 class="modal-title">Loading...</h4>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    // Fungsi untuk menampilkan modal loading
+    function showLoadingModal() {
+        var loadingModal = document.getElementById('loadingModal');
+        loadingModal.style.display = 'block';
+
+        // Simulasikan proses halaman selama 2 detik (gantilah dengan proses yang sesuai pada halaman Anda)
+        setTimeout(function () {
+            hideLoadingModal(); // Panggil fungsi untuk menyembunyikan modal setelah selesai proses
+        }, 2000); // 2000 milidetik (2 detik) adalah waktu simulasi proses halaman
+    }
+
+    // Fungsi untuk menyembunyikan modal loading
+    function hideLoadingModal() {
+        var loadingModal = document.getElementById('loadingModal');
+        loadingModal.style.display = 'none';
+    }
+</script>
 <div class="container">
     <div class="row">
         <?php
