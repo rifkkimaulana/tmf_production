b83c8335787de76fae156e7f458eb15532cc485e
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-07 00:18:15 +0700

    update

--- config/insert_log.php
+++ config/insert_log.php
@@ -1,26 +1,25 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include 'koneksi.php';
-    $username = $_POST['username'];
-    $action = $_POST['action'];
-    $description_log = $_POST['description_log'];
+include 'koneksi.php';
+$username = 'abcd';
+$action = 'tesfd';
+$description_log = 'testfasdfa123124';
 
-    $timestamp = date('Y-m-d H:i:s');
-    $ip_address = $_SERVER['REMOTE_ADDR'];
+$timestamp = date('Y-m-d H:i:s');
+$ip_address = $_SERVER['REMOTE_ADDR'];
 
-    $query_log = "INSERT INTO tb_logs_aplikasi (timestamp, username, action, description, ip_address)
+$query_log = "INSERT INTO tb_logs_aplikasi (timestamp, username, action, description, ip_address)
                     VALUES (?, ?, ?, ?, ?)";
 
-    $stmt = mysqli_prepare($koneksi, $query_log);
-    mysqli_stmt_bind_param($stmt, "sssss", $timestamp, $username, $action, $description_log, $ip_address);
+$stmt = mysqli_prepare($koneksi, $query_log);
+mysqli_stmt_bind_param($stmt, "sssss", $timestamp, $username, $action, $description_log, $ip_address);
 
-    if (mysqli_stmt_execute($stmt)) {
-        echo "Data kunjungan berhasil disimpan.";
-    } else {
-        echo "Error: " . mysqli_error($koneksi);
-    }
-
-    mysqli_stmt_close($stmt);
-    mysqli_close($koneksi);
+if (mysqli_stmt_execute($stmt)) {
+    echo "Data kunjungan berhasil disimpan.";
+} else {
+    echo "Error: " . mysqli_error($koneksi);
 }
+
+mysqli_stmt_close($stmt);
+mysqli_close($koneksi);
+
 ?>
