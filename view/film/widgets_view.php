73d654cbbc72bd966a8de41a4587f9c5f37dab0f
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-06 22:43:27 +0700

    penambahan kueri select tb_film

--- view/film/widgets_view.php
+++ view/film/widgets_view.php
@@ -9,6 +9,9 @@
                 <div class="card-body" style="max-height: 1000px; overflow-y: auto;">
                     <?php
                     include 'config/koneksi.php'; // Penambahan koneksi untuk while
+                    $query_film = "SELECT * FROM tb_film WHERE tmdb_id = '$tmdb_id'";
+                    $result_film = mysqli_query($koneksi, $query_film);
+                    $row_film = mysqli_fetch_assoc($result_film);
                     $desired_genre_ids = $row_film['genre_ids'];
                     echo $desired_genre_ids;
                     $query_film = "SELECT tb_film.thumbnail, tb_film.judul_film, tb_film.tmdb_id, tb_film.genre_ids, SUM(tb_view.jumlah_lihat) AS total_kunjungan
