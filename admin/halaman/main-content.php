3b4b0a069cc5a9508acf2f676554084044b02eed
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-07 01:58:01 +0700

    update

--- admin/halaman/main-content.php
+++ admin/halaman/main-content.php
@@ -95,6 +95,10 @@
                 }
             });
         </script>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0-alpha/dist/js/adminlte.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <!-- Modal Loading (sesuai contoh sebelumnya) -->
         <div class="modal fade" id="loadingModal" tabindex="-1" role="dialog" aria-labelledby="loadingModalLabel"
             aria-hidden="true">
