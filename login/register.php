820c1975844c4fcaec52ab4822f3c8622f63b995
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-06 20:21:21 +0700

    add comingsoon

---
+++ login/register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Coming Soon</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition">
+  <div class="wrapper">
+    <!-- Main content -->
+    <section class="content">
+      <div class="error-page">
+        <h2 class="headline text-warning">Coming Soon</h2>
+
+        <div class="error-content">
+          <h3><i class="fas fa-exclamation-triangle text-warning"></i> We are currently working on something awesome!
+          </h3>
+
+          <p>
+            Please check back later.
+          </p>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.1/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
