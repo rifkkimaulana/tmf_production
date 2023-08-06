986f6ffc25ea93d34f350a3af43b78758bb4c8da
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-06 23:50:26 +0700

    update log logout

--- logout.php
+++ logout.php
@@ -1,9 +1,26 @@
 <?php
+include 'config/base_url.php';
 session_start();
 $username = $_SESSION['username'];
-$description_log = 'Menghentikan session.';
-$action = 'logout';
-include 'config/insert_log.php';
+
+$data = array(
+    'username' => 'contoh_username',
+    'action' => 'login',
+    'description_log' => 'Mencoba Login'
+);
+
+$endpoint_url = $base_url . '/config/insert_log.php';
+$options = array(
+    'http' => array(
+        'method' => 'POST',
+        'header' => 'Content-type: application/x-www-form-urlencoded',
+        'content' => http_build_query($data)
+    )
+);
+$context = stream_context_create($options);
+$result = file_get_contents($endpoint_url, false, $context);
+
+var_dump($result);
 
 session_unset();
 session_destroy();
