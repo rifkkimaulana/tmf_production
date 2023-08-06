1855c7d6b25e214015e9dd42f5b5e605a2a15297
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-06 18:50:29 +0700

    update error widget di halaman pencarian

--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -1,22 +1,4 @@
 <?php
-$query_film_tv = "SELECT tb_film.thumbnail, tb_film.judul_film 
-                AS judul, tb_film.tmdb_id, tb_film.genre_ids, SUM(tb_view.jumlah_lihat) AS total_kunjungan
-                FROM tb_film
-                LEFT JOIN tb_view ON tb_film.tmdb_id = tb_view.tmdb_id
-                GROUP BY tb_film.tmdb_id
-
-                UNION
-
-                SELECT tb_tv_show.thumbnail, tb_tv_show.judul_tv_show 
-                AS judul, tb_tv_show.tmdb_id, tb_tv_show.genre_ids, SUM(tb_view.jumlah_lihat) AS total_kunjungan
-                FROM tb_tv_show
-                LEFT JOIN tb_view ON tb_tv_show.tmdb_id = tb_view.tmdb_id
-                GROUP BY tb_tv_show.tmdb_id
-
-                ORDER BY total_kunjungan DESC ";
-
-$result_film_tv = mysqli_query($koneksi, $query_film_tv);
-
 if (isset($_GET['search']) && !empty($_GET['search'])) {
     include 'search_movies_tv.php';
 } else {
--- view/dashboard/widgets_dashboard.php
+++ view/dashboard/widgets_dashboard.php
@@ -10,6 +10,24 @@
 
                 <div class="card-body" style="max-height: 1000px; overflow-y: auto;">
                     <?php
+                    $query_film_tv = "SELECT tb_film.thumbnail, tb_film.judul_film 
+                    AS judul, tb_film.tmdb_id, tb_film.genre_ids, SUM(tb_view.jumlah_lihat) AS total_kunjungan
+                    FROM tb_film
+                    LEFT JOIN tb_view ON tb_film.tmdb_id = tb_view.tmdb_id
+                    GROUP BY tb_film.tmdb_id
+    
+                    UNION
+    
+                    SELECT tb_tv_show.thumbnail, tb_tv_show.judul_tv_show 
+                    AS judul, tb_tv_show.tmdb_id, tb_tv_show.genre_ids, SUM(tb_view.jumlah_lihat) AS total_kunjungan
+                    FROM tb_tv_show
+                    LEFT JOIN tb_view ON tb_tv_show.tmdb_id = tb_view.tmdb_id
+                    GROUP BY tb_tv_show.tmdb_id
+    
+                    ORDER BY total_kunjungan DESC ";
+
+                    $result_film_tv = mysqli_query($koneksi, $query_film_tv);
+
                     include 'config/koneksi.php'; //koneksi tambahan untuk while
                     while ($row_film_tv = mysqli_fetch_assoc($result_film_tv)) {
 
