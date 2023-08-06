7ce319b46733d8634c58a83fba8eec54e146187d
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-07 02:35:52 +0700

    update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -46,8 +46,10 @@ if ($_SESSION['status'] != "administrator_logedin") {
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
-    <!-- Hubungkan dengan Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+
 </head>
 
 <body class="hold-transition sidebar-mini">
