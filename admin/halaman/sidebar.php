a1c1f6ec164c8be0dd3a67c5b5c085b8f9bf2b4c
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-08 23:15:12 +0700

    update bug

--- admin/halaman/sidebar.php
+++ admin/halaman/sidebar.php
@@ -310,3 +310,78 @@ $page = isset($_GET['page']) ? $_GET['page'] : '';
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="modalBerhasil" tabindex="-1" role="dialog" aria-labelledby="modalBerhasilLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalBerhasilLabel">Sukses</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Data berhasil disimpan.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modalBerhasilDihapus" tabindex="-1" role="dialog" aria-labelledby="modalBerhasilLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalBerhasilLabel">Sukses</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Data berhasil Dihapus.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modalGagal" tabindex="-1" role="dialog" aria-labelledby="modalGagalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalGagalLabel">Gagal</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Terjadi kesalahan, Perubahan data gagal ditambahkan.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    var alertType = '<?php echo isset($_GET["alert"]) ? $_GET["alert"] : ""; ?>';
+
+    $(document).ready(function () {
+        if (alertType === 'berhasil') {
+            $('#modalBerhasil').modal('show');
+        } else if (alertType === 'gagal') {
+            $('#modalGagal').modal('show');
+        } else if (alertType === 'berhasil_dihapus') {
+            $('#modalBerhasilDihapus').modal('show');
+        }
+    });
+</script>
