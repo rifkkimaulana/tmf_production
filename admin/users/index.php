33ac36491a094a2b6e325cbd7e5764c7d4179027
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-09 02:32:22 +0700

    update example user

--- admin/users/index.php
+++ admin/users/index.php
@@ -89,34 +89,5 @@
 </div>
 
 <script>
-    $(document).ready(function () {
-        var table = $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": true,
-            "searching": true,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "language": {
-                "lengthMenu": "_MENU_",
-                "zeroRecords": "No data found",
-                "info": "Showing _START_ to _END_ of _TOTAL_ entries",
-                "infoEmpty": "Showing 0 to 0 of 0 entries",
-                "infoFiltered": "(filtered from _MAX_ total entries)",
-                "search": "Cari:",
-                "paginate": {
-                    "first": "Start",
-                    "last": "End",
-                    "next": "Next",
-                    "previous": "Previous"
-                }
-            },
-            "lengthMenu": [5, 10, 50, 100],
-            "pageLength": 5
-        });
 
-        $('#selectLength').on('change', function () {
-            table.page.len($(this).val()).draw();
-        });
-    });
 </script>
