3d1151af743b2c1a7db2bc953a3e430f058de4ad
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-09 03:05:20 +0700

    update

--- admin/genre/index.php
+++ admin/genre/index.php
@@ -29,6 +29,7 @@
             <div class="col-lg-8">
                 <div class="card">
                     <form action="genre/delete.php" method="post" id="form-genre">
+
                         <div class="card-header">
                             <div class="btn-group float-right">
                                 <button type="submit" class="btn btn-danger" id="hapusGenreBtn">
@@ -38,9 +39,10 @@
 
                             <h3 class="card-title">Genre</h3>
                         </div>
+
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table id="tmf_datatable" class="table table-bordered table-striped">
+                                <table id="tmf_genreTable" class="table table-bordered table-striped">
                                     <thead>
                                         <tr>
                                             <th style="width: 2%;">
@@ -49,8 +51,10 @@
                                             <th style="width: 5%;">No</th>
                                             <th style="width: 30%;">Genre</th>
                                             <th style="width: 30%;">Slug</th>
+
                                         </tr>
                                     </thead>
+
                                     <tbody>
                                         <?php
                                         $query = "SELECT * FROM tb_genre ORDER BY id DESC";
@@ -74,7 +78,17 @@
                                 </table>
                             </div>
                         </div>
+
                     </form>
+
+
+
+                    <script>
+                        $(document).ready(function () {
+
+                        });
+                    </script>
+
                 </div>
             </div>
         </div>
