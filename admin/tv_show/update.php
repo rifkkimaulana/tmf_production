0f68f1800c00dd5ffe77ae5fb7d3cda75ff3e21a
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-09 00:16:21 +0700

    p

--- admin/tv_show/update.php
+++ admin/tv_show/update.php
@@ -99,7 +99,7 @@ include '../config/koneksi.php';
     <div class="container-fluid">
         <form action="tv_show/proses_update.php" method="post" enctype="multipart/form-data">
             <div class="row">
-                <div class="col-8">
+                <div class="col-lg-8">
                     <div class="card">
                         <div class="card-header">
                             <h3 class="card-title">Tambah TV Show</h3>
@@ -125,7 +125,7 @@ include '../config/koneksi.php';
                     </div>
                 </div>
 
-                <div class="col-4">
+                <div class="col-lg-4">
                     <div id="accordion">
                         <div class="card">
                             <div class="card-header">
