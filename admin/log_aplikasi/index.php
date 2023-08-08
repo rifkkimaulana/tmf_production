246210fa56ab68057d861924ff5662f5202e1155
rifkkimaulana <rifkkimaulana@gmail.com>
2023-08-09 02:48:13 +0700

    update

--- admin/log_aplikasi/index.php
+++ admin/log_aplikasi/index.php
@@ -1,12 +1,12 @@
 <div class="row">
-    <div class="col-12">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title">Log Aplikasi</h3>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-bordered table-striped" id="example2">
+                    <table class="table table-bordered table-striped" id="tmf_datatable">
                         <thead>
                             <tr>
                                 <th>No</th>
@@ -19,7 +19,7 @@
                         </thead>
                         <tbody>
                             <?php
-                            include '../config/koneksi.php';
+                            include_once '../config/koneksi.php';
                             $no = 1;
                             $data = mysqli_query($koneksi, "SELECT * FROM tb_logs_aplikasi ORDER BY id ASC");
                             while ($d = mysqli_fetch_assoc($data)) {
@@ -54,36 +54,3 @@
         </div>
     </div>
 </div>
-
-<script>
-    $(document).ready(function () {
-        var table = $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": true,
-            "searching": true,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "language": {
-                "lengthMenu": "_MENU_",
-                "zeroRecords": "No data found",
-                "info": "Showing _START_ to _END_ of _TOTAL_ entries",
-                "infoEmpty": "Showing 0 to 0 of 0 entries",
-                "infoFiltered": "(filtered from _MAX_ total entries)",
-                "search": "Cari:",
-                "paginate": {
-                    "first": "Start",
-                    "last": "End",
-                    "next": "Next",
-                    "previous": "Previous"
-                }
-            },
-            "lengthMenu": [5, 10, 50, 100],
-            "pageLength": 5
-        });
-
-        $('#selectLength').on('change', function () {
-            table.page.len($(this).val()).draw();
-        });
-    });
-</script>
