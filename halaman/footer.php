a84d285b28c79ff31a61dcfa1f0f25c856cac332
Rifki Maulana <76945438+rifkkimaulana@users.noreply.github.com>
2023-08-09 13:19:39 +0700

    update

--- halaman/footer.php
+++ halaman/footer.php
@@ -1,5 +1,11 @@
 </div>
 <footer class="main-footer tmf_production">
+    <p>Layarkaca21 – LK21 – Dunia21 adalah sebuah website hiburan yang menyajikan streaming film atau download movie
+        gratis. Subtitle Indonesa.
+
+        Perlu diketahui, film-film yang terdapat pada web ini didapatkan dari web pencarian di internet. Kami tidak
+        menyimpan file film tersebut di server sendiri dan kami hanya menempelkan link-link tersebut di website kami.
+    </p>
     <strong>Copyright &copy; 2014-
         <?php echo date('Y'); ?> <a style="text-transform: uppercase;" href="<?php echo $base_url; ?>">TMF
             Production</a>.
