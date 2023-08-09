1f3fe8a81bf024c30d9d4e0214f0f9c2a9dad529
Rifki Maulana <76945438+rifkkimaulana@users.noreply.github.com>
2023-08-09 10:52:55 +0700

    update sprtir by id desc

--- admin/film/index.php
+++ admin/film/index.php
@@ -26,7 +26,7 @@
                                             LEFT JOIN tb_genre ON FIND_IN_SET(tb_genre.id, tb_film.genre_ids)
                                             JOIN tb_tag ON tb_film.tag_ids = tb_tag.id
                                             GROUP BY tb_film.id
-                                            ORDER BY tb_film.id ASC";
+                                            ORDER BY tb_film.id DESC";
 
                                     $result = mysqli_query($koneksi, $query);
                                     if (!$result) {
